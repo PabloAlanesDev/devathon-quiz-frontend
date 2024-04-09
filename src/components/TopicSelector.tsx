15327c15a6a8b68286a0cdd659a494ef4727db14
Miguel Decode <migueldecode@gmail.com>
2024-04-09 20:04:06 +0200

    feat: add checkbox topics logic state

--- src/components/TopicSelector.tsx
+++ src/components/TopicSelector.tsx
@@ -1,6 +1,7 @@
 import * as Checkbox from "@radix-ui/react-checkbox";
 import { CheckIcon } from "@radix-ui/react-icons";
 import "./TopicSelector.css";
+import { useState } from "react";
 
 // TODO: Delete when have data from back
 const topics = [
@@ -17,23 +18,39 @@ const topics = [
   "Kotlin",
 ];
 
-const TopicSelector = () => (
-  <form className="border bg-slate-900 p-4 rounded-lg">
-    <div className="flex flex-col gap-4 items-start">
-      {topics.map((topic) => (
-        <div style={{ display: "flex", alignItems: "center" }}>
-          <Checkbox.Root className="CheckboxRoot" id={topic}>
-            <Checkbox.Indicator className="CheckboxIndicator">
-              <CheckIcon />
-            </Checkbox.Indicator>
-          </Checkbox.Root>
-          <label className="Label" htmlFor={topic}>
-            {topic}
-          </label>
-        </div>
-      ))}
-    </div>
-  </form>
-);
+const TopicSelector = () => {
+  const [checkedTopics, setCheckedTopics] = useState([]);
+
+  const handleTopics = (checked: boolean | string, topic: string) => {
+    const newTopics = checked
+      ? [...checkedTopics, topic]
+      : checkedTopics.filter((el) => el !== topic);
+
+    setCheckedTopics(newTopics);
+  };
+
+  return (
+    <form className="border bg-slate-900 p-4 rounded-lg">
+      <div className="flex flex-col gap-4 items-start">
+        {topics.map((topic) => (
+          <div key={topic} className="flex items-center">
+            <Checkbox.Root
+              className="CheckboxRoot"
+              onCheckedChange={(checked) => handleTopics(checked, topic)}
+              id={topic}
+            >
+              <Checkbox.Indicator className="CheckboxIndicator">
+                <CheckIcon />
+              </Checkbox.Indicator>
+            </Checkbox.Root>
+            <label className="Label" htmlFor={topic}>
+              {topic}
+            </label>
+          </div>
+        ))}
+      </div>
+    </form>
+  );
+};
 
 export default TopicSelector;

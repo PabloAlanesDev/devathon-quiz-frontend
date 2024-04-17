1d79e856e51df9e76073aa95bed030a528ea93ae
Miguel Decode <migueldecode@gmail.com>
2024-04-17 22:44:49 +0200

    chore: add react-router

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,22 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { Theme } from '@radix-ui/themes';
+import { Theme } from "@radix-ui/themes";
 import App from "./App.tsx";
 import "./globals.css";
 import "@radix-ui/themes/styles.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Theme>
-      <App />
+      <RouterProvider router={router}></RouterProvider>
     </Theme>
   </StrictMode>
 );

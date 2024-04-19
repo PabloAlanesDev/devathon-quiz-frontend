968d43a384640cdc3997f981536d45ed6ea29c94
luis-tenorio-code <luisprograming0@gmail.com>
2024-04-19 13:34:39 -0500

    feat: add page results

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,25 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { Theme } from "@radix-ui/themes";
-import App from "./App.tsx";
-import "./globals.css";
-import "@radix-ui/themes/styles.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import CreateRoomPage from "./pages/CreateRoomPage.tsx";
-import JoinRoomPage from "./pages/JoinRoomPage.tsx";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Theme } from '@radix-ui/themes';
+import App from './App.tsx';
+import './globals.css';
+import '@radix-ui/themes/styles.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import CreateRoomPage from './pages/CreateRoomPage.tsx';
+import JoinRoomPage from './pages/JoinRoomPage.tsx';
+import ResultsPage from './pages/results/Results.tsx';
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <App />,
   },
-  { path: "/create", element: <CreateRoomPage /> },
-  { path: "/join", element: <JoinRoomPage /> },
+  { path: '/create', element: <CreateRoomPage /> },
+  { path: '/join', element: <JoinRoomPage /> },
+  { path: '/results', element: <ResultsPage /> },
 ]);
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Theme>
       <RouterProvider router={router}></RouterProvider>
